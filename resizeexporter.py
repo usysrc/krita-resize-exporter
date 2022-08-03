833eb468fa6418a4df71323e7ade57d3164dd7d2
usysrc <usysrc@usysrc.de>
2022-08-03 18:18:40 +0200

    added files

---
+++ resizeexporter.py
@@ -0,0 +1,16 @@
+from krita import *
+
+class MyExtension(Extension):
+
+    def __init__(self, parent):
+        # This is initialising the parent, always important when subclassing.
+        super().__init__(parent)
+
+    def setup(self):
+        pass
+
+    def createActions(self, window):
+        action = window.createAction("myAction", "My Script", "tools/scripts")
+
+# And add the extension to Krita's list of extensions:
+Krita.instance().addExtension(MyExtension(Krita.instance()))
